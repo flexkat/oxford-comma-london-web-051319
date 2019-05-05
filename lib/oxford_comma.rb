e43c4613bf52b02226a4dbdc7251f02ab9fbf917
Catherine Maritz <catherinemaritz1@gmail.com>
2019-05-05 13:45:27 +0100

    oxford comma method

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(' and ')
+  elsif array.size >= 3
+      last_item = array.pop
+      last_item = ('and ').concat(last_item)
+      array << last_item
+      # array.join(', ') #now a string 
+      # new_string = array + ", and" + (last_item) 
+      array.join(", ")
+  end
 end
